c6e9cc23a8d62cfd0e1d928424c22d8e0760d20b
masato sasaki <m.tobcreation@gmail.com>
2021-11-18 15:50:53 +0900

    no message

--- app/app.py
+++ app/app.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 #「/」へアクセスがあった場合に、"Hello World"の文字列を返す
 @app.route("/")
 def hello():
-    return "Hello python"
+    return render_template("index.html")
 
 
 #「/index」へアクセスがあった場合に、「index.html」を返す
-@app.route("/index")
+@app.route("/login")
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
 
 
 #おまじない
