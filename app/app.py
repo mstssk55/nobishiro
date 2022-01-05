b915bfca5da4133c1a608d09b350d327a381ed5a
masato sasaki <m.tobcreation@gmail.com>
2022-01-05 17:07:24 +0900

    環境変数の設定

--- app/app.py
+++ app/app.py
@@ -1,15 +1,17 @@
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
 from flask import Flask,render_template,request
 import sqldb as db
+import setting as s
 #Flaskオブジェクトの生成
 app = Flask(__name__)
-
 
 #「/」へアクセスがあった場合に、"Hello World"の文字列を返す
 @app.route("/")
 def hello():
     data = db.get_iot_data()
-    return render_template("index.html",data=data)
+    room = s.room_number
+    d_type = s.device_type
+    return render_template("index.html",data=data,room=room,d_type=d_type)
 
 
 #「/index」へアクセスがあった場合に、「index.html」を返す
